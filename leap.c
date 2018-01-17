cdd26d34b581630350d747ef0d0e3453d2112f40
sabud <35525196+sabud@users.noreply.github.com>
2018-01-17 16:34:25 +0530

    Create leap.c

---
+++ leap.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(void)
+{
+int n;
+printf("enter the vlues");
+scanf("%d",&n);
+if(n%4==0)
+{
+printf("it is a leap yr");
+}
+else
+{
+printf("it is not a leap yr");
+}
+return 0;
+}
